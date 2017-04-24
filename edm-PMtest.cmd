bbb21f52765d6d2d1b20277bf1b83d9373c6e2ba
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2017-04-24 12:22:12 -0700

    Result after running patches/deleting_edm-PMtest.patch

--- edm-PMtest.cmd
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-export EDMDATAFILES="$EDMDATAFILES:.:..:Bx9000_MBTApp/srcDisplay"
-
-edm -x -m "apc=LCLS:PM:BHC:TEST" -eolc Bx9000_MBTApp/srcDisplay/Bx9000Info-lcls.edl Bx9000_MBTApp/srcDisplay/Bx9000InstalledBTs-lcls.edl &
-edm -x -m "DOUT=TEST:PM:DOUT:,LOC='B34 racks'" -eolc Bx9000_MBTApp/srcDisplay/do.edl &
-edm -x -m "DIN=TEST:PM:DIN:,LOC=B34 racks" -eolc Bx9000_MBTApp/srcDisplay/di.edl &
