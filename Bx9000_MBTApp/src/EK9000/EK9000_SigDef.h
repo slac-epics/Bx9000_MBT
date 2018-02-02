03aac1da4bdeb36e05140f16571729f24c2a0091
Scott Stubbs <sstubbs@slac.stanford.edu>
2018-02-01 23:46:04 -0800

    See previous commit.

---
+++ Bx9000_MBTApp/src/EK9000/EK9000_SigDef.h
@@ -0,0 +1,55 @@
+#ifndef	_EK9000_SIGDEF_H_
+#define	_EK9000_SIGDEF_H_
+
+#include "EK9000_MBT_Common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+#if	0	/* Must be 0, just for reference here */
+/******************************************************************************************/
+/* The signal definition, the busterm_string/type and function combination must be unique */
+/* The definition of arg1 -> arg3 is different between different operation                */
+/*	BT_OPTYPE_READ_INPUT_CIMG/BT_OPTYPE_READ_OUTPUT_CIMG/BT_OPTYPE_WRITE_OUTPUT_CIMG: */
+/* word offset to start of terminal, number of words, effective bits                      */
+/*	BT_OPTYPE_READ_INPUT_DIMG/BT_OPTYPE_READ_OUTPUT_DIMG/BT_OPTYPE_WRITE_OUTPUT_DIMG: */
+/* bit offset to start of terminal, number of bits, 0                                     */
+/*	BT_OPTYPE_READ_CPLR_MREG/BT_OPTYPE_WRITE_CPLR_MREG:                               */
+/* word offset in image above COUPLER_MREG_START, Reset to apply, 0?                      */
+/*	BT_OPTYPE_READ_CPLR_REG/BT_OPTYPE_WRITE_CPLR_REG:                                 */
+/* table, register, Reset to apply?                                                       */
+/*      BT_OPTYPE_CPLR_DIAG                                                               */
+/* sub-function, 0, 0                                                                     */
+/*	BT_OPTYPE_READ_TERM_REG/BT_OPTYPE_WRITE_TERM_REG:                                 */
+/* C/S word offset in complex input image, C/S word offset in complex output image, reg   */ 
+/******************************************************************************************/
+typedef	struct BUSTERM_SIG_PREDEF
+{/* We use this one to define signals */
+	UINT8				busterm_string[8];		/* String name of bus terminal, must be 6 characters */
+	E_BUSTERM_TYPE			busterm_type;			/* number name of bus terminal */
+	UINT8				function[MAX_CA_STRING_SIZE];	/* Function name, should be the third part of INP/OUT field */
+	E_BUSTERM_OPTYPE		busterm_optype;			/* The operation type, one of above list */
+	E_EPICS_RTYPE			epics_rtype;			/* EPICS record type to use this signal, EPICS_RTYP_NONE means not for EPICS or waive check */
+	E_DATA_TYPE			data_type;
+	UINT32				arg1;
+	UINT32				arg2;
+	UINT32				arg3;
+}	BUSTERM_SIG_PREDEF;
+#endif
+
+/* Most of signals could be processed by default function, but if you have anything longer than 16 bits, you might need your own function */
+static	BUSTERM_SIG_PREDEF	busterm_sig_predef[]= {
+
+	{"EL5042", BT_TYPE_EL5042, "R_ANA_IN_1",        BT_OPTYPE_READ_INPUT_CIMG,      EPICS_RTYPE_AI, DTYP_SINT16,    1, 1, 16},
+	{"EL5042", BT_TYPE_EL5042, "R_ANA_IN_2",        BT_OPTYPE_READ_INPUT_CIMG,      EPICS_RTYPE_AI, DTYP_SINT16,    3, 1, 16},
+
+};
+
+#define	N_BT_SIG_PREDEF	(sizeof(busterm_sig_predef)/sizeof(BUSTERM_SIG_PREDEF))
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif
---
+++ Bx9000_MBTApp/src/EK9000/EK9000_Watchdog.h
@@ -0,0 +1,105 @@
+#ifndef		_EK9000_WATCHDOG_H_
+#define		_EK9000_WATCHDOG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* __cplusplus */
+
+/**
+ * These Beckhoff registers are 16 bits wide
+ */
+static const unsigned short int	EK_REG_COUPLER_STATUS		=	0x100C;	/* Read only */
+static const unsigned short int	EK_REG_WATCHDOG_CURTIME		=	0x1020;	/* Read only */
+static const unsigned short int	EK_REG_WATCHDOG_TIMEOUT		=	0x1120;
+static const unsigned short int	EK_REG_WATCHDOG_RESET		=	0x1121;
+static const unsigned short int	EK_REG_WATCHDOG_TYPE		=	0x1122;
+
+static const unsigned short int	EK_STATUS_WATCHDOG_ELAPSED	=	0x8000;
+static const unsigned short int	EK_STATUS_CONFIG_ERROR		=	0x0002;
+static const unsigned short int	EK_STATUS_BUS_TERM_ERROR	=	0x0001;
+
+static const unsigned short int	EK_WATCHDOG_RESET_WORD_0	=	0xBECF;
+static const unsigned short int	EK_WATCHDOG_RESET_WORD_1	=	0xAFFE;
+
+
+/**
+ *	EK_CheckWatchdog
+ *	Checks the status of the watchdog timer.
+ *	If fQuiet is FALSE or 0, it prints status to stdout
+ *	Returns TRUE if watchdog has elapsed, FALSE if OK
+ */
+extern unsigned int EK_CheckWatchdog( char * pNameOrIpAddr, unsigned int fQuiet );
+
+/**
+ *	MBT_CheckWatchdog
+ *	Checks the status of the watchdog timer.
+ *	If fQuiet is FALSE or 0, it prints status to stdout
+ *	Returns TRUE if watchdog has elapsed, FALSE if OK
+ */
+extern unsigned int MBT_CheckWatchdog( ModBusTCP_Link mbt_link, unsigned int fQuiet );
+
+/**
+ *	EK_SetWatchdogType
+ *	Sets the type to either R/W or Write mode.
+ *	R/W mode resets the watchdog timer on either a read or a write telegram.
+ *	Write mode resets the watchdog timer only on write telegrams.
+ *	(A telegram is a message reading or writing a process value);
+ *	tyWatchdog = 1 for R/W Telegram Mode
+ *	tyWatchdog = 0 for Write Telegram Mode
+ */
+extern int EK_SetWatchdogType( char * pNameOrIpAddr, unsigned int tyWatchdog );
+
+/**
+ *	MBT_SetWatchdogType
+ *	Sets the type to either R/W or Write mode.
+ *	R/W mode resets the watchdog timer on either a read or a write telegram.
+ *	Write mode resets the watchdog timer only on write telegrams.
+ *	(A telegram is a message reading or writing a process value);
+ *	tyWatchdog = 1 for R/W Telegram Mode
+ *	tyWatchdog = 0 for Write Telegram Mode
+ */
+extern int MBT_SetWatchdogType( ModBusTCP_Link mbt_link, unsigned int tyWatchdog );
+
+/**
+ *	EK_ResetWatchdog
+ *	Resets the watchdog timer
+ */
+extern int EK_ResetWatchdog( char * pNameOrIpAddr );
+
+/**
+ *	MBT_ResetWatchdog
+ *	Resets the watchdog timer
+ */
+extern int MBT_ResetWatchdog( ModBusTCP_Link mbt_link );
+
+/**
+ *	EK_SetWatchdogTimer
+ *	Set the watchdog timer to the desired timeout in ms
+ */
+extern int EK_SetWatchdogTimeout( char * pNameOrIpAddr, unsigned int timeout );
+
+/**
+ *	MBT_SetWatchdogTimer
+ *	Set the watchdog timer to the desired timeout in ms
+ */
+extern int MBT_SetWatchdogTimeout( ModBusTCP_Link mbt_link, unsigned int timeout );
+
+/**
+ *	EK_DisableWatchdog
+ *	Disable the watchdog timer by setting the timeout to zero
+ *	To re-enable, use EK_SetWatchdogTimeout()
+ */
+extern int EK_DisableWatchdog( char * pNameOrIpAddr );
+
+/**
+ *	MBT_DisableWatchdog
+ *	Disable the watchdog timer by setting the timeout to zero
+ *	To re-enable, use MBT_SetWatchdogTimeout()
+ */
+extern int MBT_DisableWatchdog( ModBusTCP_Link mbt_link );
+
+#ifdef __cplusplus
+}
+#endif  /* __cplusplus */
+
+#endif	/*	_EK9000_WATCHDOG_H_	*/
