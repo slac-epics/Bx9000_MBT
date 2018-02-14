e9ecb75d035ee38e0d0c9c3852dd31703f7ec840
Scott Stubbs <sstubbs@slac.stanford.edu>
2018-02-13 18:18:46 -0800

    EL5042 record should be longin rather than ai. Started conversion.

--- Bx9000_MBTApp/srcEtherCat/devEL5042.c
+++ Bx9000_MBTApp/srcEtherCat/devEL5042.c
@@ -1,85 +1,86 @@
 #include <EK9000_MBT_Common.h>
-#include <aiRecord.h>
+#include <longinRecord.h>
+//TODO: change to long input record
 /* EL5042 is 2-channel BiSS-C terminal with configurable serial params and resolution up to 64-bit */
 
 
 extern	SINT32	EK9000_DEV_DEBUG;
 	
-static long init_ai_EL5042(struct aiRecord * pai)
+static long init_li_EL5042(struct longinRecord * pli)
 {
-	if (pai->inp.type!=INST_IO)
+	if (pli->inp.type!=INST_IO)
 	{
-		recGblRecordError(S_db_badField, (void *)pai,
-			"devAiEL5042 Init_record, Illegal INP");
-		pai->pact=TRUE;
+		recGblRecordError(S_db_badField, (void *)pli,
+			"devLiEL5042 Init_record, Illegal INP");
+		pli->pact=TRUE;
 		return (S_db_badField);
 	}
 
-	if(EK9000_Signal_Init((dbCommon *) pai, EPICS_RTYPE_AI, pai->inp.value.instio.string, BT_TYPE_EL5042, EK9000_Dft_ProcFunc, NULL) != 0)
+	if(EK9000_Signal_Init((dbCommon *) pli, EPICS_RTYPE_AI, pli->inp.value.instio.string, BT_TYPE_EL5042, EK9000_Dft_ProcFunc, NULL) != 0)
 	{
-		if(EK9000_DEV_DEBUG)	errlogPrintf("Fail to init signal for record %s!!", pai->name);
-		recGblRecordError(S_db_badField, (void *) pai, "Init signal Error");
-		pai->pact = TRUE;
+		if(EK9000_DEV_DEBUG)	errlogPrintf("Fail to init signal for record %s!!", pli->name);
+		recGblRecordError(S_db_badField, (void *) pli, "Init signal Error");
+		pli->pact = TRUE;
 		return (S_db_badField);
 	}
 
-	pai->eslo = (pai->eguf - pai->egul)/(float)0x10000;
-	pai->roff = 0x8000;
+	pli->eslo = (pli->eguf - pli->egul)/(float)0x10000;
+	pli->roff = 0x8000;
 
 	return 0;
 }
 
-static long read_ai_EL5042(struct aiRecord * pai)
+static long read_li_EL5042(struct longinRecord * pli)
 {
-	EK9000_SIGNAL	* psignal = (EK9000_SIGNAL *) (pai->dpvt);
+	EK9000_SIGNAL	* psignal = (EK9000_SIGNAL *) (pli->dpvt);
 
-	if (!pai->pact)
+	if (!pli->pact)
 	{
 		if(epicsMessageQueueTrySend(psignal->pdevdata->pcoupler->msgQ_id, (void *)&psignal, sizeof(EK9000_SIGNAL *)) == -1)
 		{
-			recGblSetSevr(pai, READ_ALARM, INVALID_ALARM);
-			if(EK9000_DEV_DEBUG)	errlogPrintf("Send Message to Operation Thread Error [%s]", pai->name);
+			recGblSetSevr(pli, READ_ALARM, INVALID_ALARM);
+			if(EK9000_DEV_DEBUG)	errlogPrintf("Send Message to Operation Thread Error [%s]", pli->name);
 			return -1;
 		}
 		else
 		{
-			pai->pact = TRUE;
+			pli->pact = TRUE;
 		}
 	}
 	else
 	{
 		if ( (!psignal->pdevdata->op_done) || psignal->pdevdata->err_code )
 		{
-			recGblSetSevr(pai, READ_ALARM, INVALID_ALARM);
-			if(EK9000_DEV_DEBUG)	errlogPrintf("Record [%s] receive error code [0x%08x]!\n", pai->name, psignal->pdevdata->err_code);
+			recGblSetSevr(pli, READ_ALARM, INVALID_ALARM);
+			if(EK9000_DEV_DEBUG)	errlogPrintf("Record [%s] receive error code [0x%08x]!\n", pli->name, psignal->pdevdata->err_code);
 			return -1;
 		}
 		else
 		{
-			pai->udf = FALSE;
+			pli->udf = FALSE;
 //  Commenting out these lines since there is no feature register in EtherCAT.  Will clean up later.
 			/* psignal->pdevdata->pbusterm_sig_def->data_type is a little bit overkill */
 			/* We don't check term_reg_exist, because we know we do */
 //			if(psignal->pdevdata->pcoupler->installedBusTerm[psignal->pdevdata->slot].term_r32_value & 0x8)/* check bit 3 */
 //			{/* signed amount */
-//				pai->rval = ( (psignal->pdevdata->value)&0x7FFF ) * ( ((psignal->pdevdata->value)&0x8000)?-1:1 );
+//				pli->rval = ( (psignal->pdevdata->value)&0x7FFF ) * ( ((psignal->pdevdata->value)&0x8000)?-1:1 );
 //			}
 //			else
 //			{/* two's complement */
-//				pai->rval = (SINT16)(psignal->pdevdata->value);
+//				pli->rval = (SINT16)(psignal->pdevdata->value);
 //			}
 		}
 	}
 	return (CONVERT);
 }
 
-static long lincvt_ai_EL5042(struct aiRecord	*pai, int after)
+static long lincvt_li_EL5042(struct longinRecord *pli, int after)
 {
 
 	if(!after) return(0);
 	/* set linear conversion slope*/
-	pai->eslo = (pai->eguf - pai->egul)/(float)0x10000;
-	pai->roff = 0x8000;
+	pli->eslo = (pli->eguf - pli->egul)/(float)0x10000;
+	pli->roff = 0x8000;
 	return(0);
 }
 
@@ -88,19 +89,19 @@ struct {
 	long            number;
 	DEVSUPFUN       report;
 	DEVSUPFUN       init;
-	DEVSUPFUN       init_ai;
+	DEVSUPFUN       init_li;
 	DEVSUPFUN       get_ioint_info;
-	DEVSUPFUN       read_ai;
+	DEVSUPFUN       read_li;
 	DEVSUPFUN       special_linconv;
-}	devAiEL5042 =
+}	devLiEL5042 =
 {
 	6,
 	NULL,
 	NULL,
-	init_ai_EL5042,
+	init_li_EL5042,
 	NULL,
-	read_ai_EL5042,
-	lincvt_ai_EL5042
+	read_li_EL5042,
+	lincvt_li_EL5042
 };
-epicsExportAddress(dset, devAiEL5042);
+epicsExportAddress(dset, devLiEL5042);
 
