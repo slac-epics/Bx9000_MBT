ecf6d4dd233c1181eadccb00e8ed5159018e2bd6
Marcio Paduan Donadio <marcio@slac.stanford.edu>
2019-10-25 15:55:12 -0700

    Added missing modules that were lost during the migration from CVS to GIT

--- Bx9000_MBTApp/src/Bk9000_BTDef.h
+++ Bx9000_MBTApp/src/Bk9000_BTDef.h
@@ -80,8 +80,11 @@ static BUSTERM_IMG_DEF	busterm_img_def[]={
 	{"KL2808",      BT_TYPE_KL2808, 0,      0,      0,      0,      0,      8},
 	{"KL2512",      BT_TYPE_KL2512, 1,      0x0004, 4,      4,      0,      0},
 	{"KL2622",      BT_TYPE_KL2622, 0,      0,      0,      0,      0,      2},
+	{"KL2872",      BT_TYPE_KL2872, 0,      0,      0,      0,      0, 		16},	
 	{"KL3064",      BT_TYPE_KL3064, 1,      0x1106, 8,      8,      0,      0},
 	{"KL3102",		BT_TYPE_KL3102,	1,		0x0000,	4,		4,		0,		0},
+	{"KL3022",		BT_TYPE_KL3022,	1,		0x0000,	4,		4,		0,		0},		
+	{"KL3012",		BT_TYPE_KL3012,	1,		0x0000,	4,		4,		0,		0},
 /* per files Access from User Program-Register Overveiw.pdf and Access from User Program-Mapping.pdf; "Complete evaluation in Intel format with word alignment" --AC */
     {"KL3162",      BT_TYPE_KL3162, 1,      0x0180, 4,      4,      0,      0},
 /* per KL320xen.pdf, top of p. 14 -- AC */
@@ -99,7 +102,10 @@ static BUSTERM_IMG_DEF	busterm_img_def[]={
 	{"KL3468",      BT_TYPE_KL3468, 1,      0x1106, 16,     16,     0,      0},
 	{"KL3464",      BT_TYPE_KL3464, 1,      0x1106, 8,     8,     0,      0},
 	{"KL4002",		BT_TYPE_KL4002,	1,		0x0006,	4,		4,		0,		0},
+	{"KL4004",	BT_TYPE_KL4004,	1,	0x0006,	8,	 8,	0,	0},
 	{"KL4132",		BT_TYPE_KL4132,	1,		0x0006,	4,		4,		0,		0},
+	{"KM1004",      BT_TYPE_KM1004, 0,      0,      0,      0,      32,     0},		
+	{"KM2004",      BT_TYPE_KM2004, 0,      0,      0,      0,      0, 		32},
 	{"KL9505",		BT_TYPE_KL9505,	0,	0,	0,	0,	0,	0},	/* Power Supply Module */
 	{"KL9020",		BT_TYPE_KL9020,	0,	0,	0,	0,	0,	0},
 	{"KL9080",		BT_TYPE_KL9080,	0,	0,	0,	0,	0,	0}, /* Separation terminal, just a blank */
